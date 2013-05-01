891d8568d78bb580805ce5966c12496863b91cdc
Wes Okes <wes.okes@gmail.com>
2013-05-01 15:42:27 -0400

    * Added placeholders for apis

--- test_project/controllers.py
+++ test_project/controllers.py
@@ -1,4 +1,8 @@
+import json
+from pprint import pprint
 from redokes.controllers.base import Api, Action
+from redokes.controllers.thirdparty import LastFm, GitHub
+import requests
 
 
 class TestingAround(Api):
@@ -14,4 +18,36 @@ class Wes(Action):
 
     def index__action(self):
         pass
+
+
+class LastFmController(LastFm):
+
+    def init_defaults(self):
+        super(LastFmController, self).init_defaults()
+
+
+    def index__action(self):
+        self.set_response_param('api_key', self.api_key)
+        response = requests.get(self.api_url)
+
+
+class GitHubController(GitHub):
+
+    def init_defaults(self):
+        super(GitHubController, self).init_defaults()
+
+
+    def index__action(self):
+        api_url = 'https://api.github.com/user/repos'
+        response = requests.get(
+            api_url,
+            params={
+                'sort': 'pushed',
+                'direction': 'desc'
+            },
+            auth=(
+                
+            )
+        )
+        self.set_response_param('response', json.loads(response.text))
 
